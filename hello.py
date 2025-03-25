864d7f582a9f157481a42bb3594315adf5bb5071
sgykkkkk <9223356@qq.com>
2025-03-25 14:26:07 +0800

    message 6:hello test now checks we can input a to-do item

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -10,19 +13,35 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 张三听说有一个在线待办事项的应用
-        # 他去看了这个应用的首页
+        # 用户听说有一个在线待办事项应用
+        # 他去打开了这个应用的首页
         self.browser.get('http://localhost:8000')
 
-        # 他注意到网页里包含"To-Do"这个词
-        self.assertEqual('To-Do lists', self.browser.title, "browser title was:" + self.browser.title)
+        # 他注意到网页标题中包含“To-Do”这个词
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 应用中有一个输入框，提示输入一个待办事项
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 他在文本输入框中输入了 "Buy flowers"
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
+
         self.fail('Finish the test!')
-
-        # 应用有一个输入待办事项的文本输入框
-        # 他在文本输入框中输入了"Buy flowers"
-
-        # 他访问那个URL，发现他的待办事项列表还在
-        # 他满意的离开了
+        # 他访问的页面URL，发现他的待办事项列表还在
+        
+        # 他感到很满意
 
 if __name__ == '__main__':
     unittest.main()
