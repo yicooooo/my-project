f98ddbb9f8bbacbf013c0cfa205b875023f82abd
sgykkkkk <9223356@qq.com>
2025-04-29 13:14:56 +0800

    message19:finish 14 chapter

--- notes/urls.py
+++ notes/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path,re_path
-from lists import views
+from django.urls import path, re_path, include
+from lists import views as list_views  # (1)
+from lists import urls as list_urls  # (1)
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new',views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    path('', list_views.home_page, name='home'),
+    path('lists/', include(list_urls)),  # (2)
 ]
