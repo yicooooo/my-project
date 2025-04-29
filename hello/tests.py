6d034dc1331a1e1bfb664784b89ba076a24e682f
sgykkkkk <9223356@qq.com>
2025-04-29 17:13:35 +0800

    message 22:use Bootstrap to improve layout

--- hello/tests.py
+++ hello/tests.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
+import unittest 
 from selenium.webdriver.common.by import By
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
